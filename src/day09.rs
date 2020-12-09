8b4732f57301e2d8866d5e00c5874e9edfde8451
silverdrake11 <kevinnasto@gmail.com>
2020-12-09 15:36:42 -0600

    Fixed a couple things in solution

--- src/day09.rs
+++ src/day09.rs
@@ -14,24 +14,14 @@ fn check_num(num: &usize, set: &HashSet<usize>) -> bool {
   return false
 }
 
-fn get_answer(cur_idx: usize, window: usize, xmas: &Vec<usize>) -> usize{
-  let slice = &xmas[cur_idx+1-window..cur_idx+1];
-  return *slice.iter().min().unwrap() + *slice.iter().max().unwrap();
-}
-
 fn find_sum(num: usize, window: usize, xmas: &Vec<usize>) -> Option<usize> {
-  let mut sum: usize = xmas[0..window].iter().sum();
-  if num == sum {
-    return Some(get_answer(window, window, xmas));
-  }
-  for i in window..xmas.len() {
-    sum -= xmas[i-window];
-    sum += xmas[i];
-    if num == sum {
-      return Some(get_answer(i, window, xmas));
+  for slice in xmas.windows(window) {
+    if num == slice.iter().sum() {
+      let answer = *slice.iter().min().unwrap() + *slice.iter().max().unwrap();
+      return Some(answer);
     }
   }
-  return None
+  return None;
 }
 
 pub fn day09() {
@@ -61,11 +51,11 @@ pub fn day09() {
       for window in 2..v.len() {
         if let Some(answer) = find_sum(cur_num, window, &v) {
           println!("Part2) {}", answer);
+          break;
         }
       }
       break;
     }
-
     set.remove(&to_remove);
     set.insert(cur_num);
   }
