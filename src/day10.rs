7c2a3a60ff5135f2668d7f895b9cb5389aa62f1b
silverdrake11 <kevinnasto@gmail.com>
2020-12-10 22:55:45 -0600

    Aoc part 1 day 10

--- src/day10.rs
+++ src/day10.rs
@@ -1,43 +1,5 @@
 use std::fs;
-use std::collections::HashSet;
 
-#[derive(PartialEq,Clone,Copy)]
- enum Op {
-    Jmp,
-    Nop,
-    Acc,
-    Err,
-}
-
-fn run(prog: &Vec<(Op,i16)>) {
-
-  let mut i: i16 = 0;
-  let mut acc: i16 = 0;
-  let mut set: HashSet<i16> = HashSet::new();
-
-  loop {
-
-    if (i as usize) >= prog.len() {
-      println!("{:?}", acc);
-      break
-    }
-
-    let op = prog[i as usize].0.clone();
-    let arg = prog[i as usize].1;
-    if set.contains(&i) {
-      println!("{:?}", acc);
-      break
-    }
-    set.insert(i);
-
-    match op {
-      Op::Acc => acc += arg,
-      Op::Jmp => i += arg - 1,
-      _ => (),
-    }
-    i += 1;
-  }
-}
 
 pub fn day10() {
 
@@ -45,20 +7,43 @@ pub fn day10() {
 
   let contents = fs::read_to_string(filename).unwrap();
 
-  let mut prog: Vec<(Op,i16)> = Vec::new();
+  let mut adapters: Vec<usize> = Vec::new();
+  adapters.push(0);
 
   for line in contents.lines() {
-    let inst: Vec<&str> = line.split_whitespace().collect();
-    let arg: i16 = inst[1].parse::<i16>().unwrap();
-    let op = match inst[0] {
-      "acc" => Op::Acc,
-      "jmp" => Op::Jmp,
-      "nop" => Op::Nop,
-      _ => Op::Err,
-    };
-    prog.push((op, arg));
+    let x = line.parse::<usize>().unwrap();
+    adapters.push(x);
   }
 
-  run(&prog);
+  adapters.sort();
+  adapters.push(adapters.iter().last().unwrap() + 3);
+
+  let mut count1 = 0;
+  let mut count3 = 0;
+
+  let mut diffs: Vec<usize> = Vec::new();
+
+  for slice in adapters.windows(2) {
+    let diff = slice[1] - slice[0];
+    println!("{} {}", diff, slice[0]);
+    match diff {
+      1 => count1 += 1,
+      3 => count3 += 1,
+      _ => println!("Error!"),
+    }
+    diffs.push(diff);
+  }
+
+  let mut count = 0;
+  for slice in diffs.windows(3) {
+    if slice[0] == 1 && slice[1] == 1 {
+      if slice[2] == 1 || slice[2] == 3 {
+        count += 1;
+      }
+    }
+  }
+
+  let n: usize = 2;
+  println!("{} {}", count1 * count3, n.pow(count));
  
 }
