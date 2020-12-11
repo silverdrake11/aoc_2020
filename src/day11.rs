608f1b86ecd0b5e459427b03fb97f3e1288d1f76
silverdrake11 <kevinnasto@gmail.com>
2020-12-11 02:54:48 -0600

    Added solution for part 2

--- src/day11.rs
+++ src/day11.rs
@@ -1,7 +1,8 @@
 use std::fs;
 use std::collections::HashMap;
+use std::collections::HashSet;
 
-fn get_changes(point: (usize, usize), rows: &Vec<Vec<char>>) -> Option<char>{
+fn get_changes_part1(point: (usize, usize), rows: &Vec<Vec<char>>) -> Option<char>{
   let (y, x) = point;
 
   let seat = rows[y][x];
@@ -45,6 +46,67 @@ fn get_changes(point: (usize, usize), rows: &Vec<Vec<char>>) -> Option<char>{
   return None
 }
 
+fn get_changes_part2(point: (usize, usize), rows: &Vec<Vec<char>>) -> Option<char>{
+  let (y, x) = point;
+
+  let seat = rows[y][x];
+
+  if seat == '.' {
+    return None
+  }
+
+  let ymax = rows.len();
+  let xmax = rows[0].len();
+
+  let mut occupied: usize = 0;
+  let mut dirs: HashSet<(i32,i32)> = vec![(0,1),(1,0),(1,1),(0,-1),(-1,0),(-1,-1),(-1,1),(1,-1)].into_iter().collect();
+  let mut r: i32 = 1;
+  while dirs.len() > 0 {
+    let mut to_remove: Vec<(i32,i32)> = Vec::new();
+    for &dir in &dirs {
+      let dy = dir.0 * r;
+      let dx = dir.1 * r;
+      if dx + (x as i32) < 0 {
+        to_remove.push(dir);
+        continue;
+      }
+      if dy + (y as i32) < 0 {
+        to_remove.push(dir);
+        continue;
+      }
+      let yadj = (y as i32 + dy) as usize;
+      let xadj = (x as i32 + dx) as usize;
+      if yadj >= ymax || xadj >= xmax {
+        to_remove.push(dir);
+        continue;
+      }
+      let seat_adj = rows[yadj][xadj];
+      if seat_adj == 'L' {
+        to_remove.push(dir);
+        continue;
+      }
+      if seat_adj == '#' {
+        occupied += 1;
+        to_remove.push(dir);
+      }
+      if seat == '#' && occupied >= 5 {
+        return Some('L');
+      }
+      if seat == 'L' && occupied > 0 {
+        return None;
+      }
+    }
+    for el in to_remove {
+      dirs.remove(&el);
+    }
+    r += 1;
+  }
+  if seat == 'L' && occupied == 0 {
+    return Some('#')
+  }
+  return None
+}
+
 pub fn day11() {
 
   let filename: String = "11.txt".to_string();
@@ -60,7 +122,7 @@ pub fn day11() {
     let mut changes: HashMap<(usize,usize),char> = HashMap::new();
     for y in 0..rows.len() {
       for x in 0..rows[0].len() {
-        if let Some(seat) = get_changes((y,x), &rows) {
+        if let Some(seat) = get_changes_part2((y,x), &rows) {
           changes.insert((y,x),seat);
         }
       }
