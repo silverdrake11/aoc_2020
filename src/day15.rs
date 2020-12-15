836c452caa63bee292b878cf2b7f7d69e6591031
silverdrake11 <kevinnasto@gmail.com>
2020-12-15 01:26:12 -0600

    Made it faster

--- src/day15.rs
+++ src/day15.rs
@@ -1,28 +1,23 @@
-use std::collections::HashMap;
 
 pub fn day15() {
 
   let v: Vec<usize> = vec![1,0,18,10,19,6];
-  let mut nums_spoken: HashMap<usize,usize> = HashMap::new();
-  //v.iter().enumerate().map(|(i,&n)| nums_spoken.insert(n,i)).collect();
-  for i in 0..v.len() {
-    nums_spoken.insert(v[i],i+1);
+  let input_len = v.len();
+  let max: usize = 30000000;
+  let mut nums_spoken: Vec<usize> = vec![0; max];
+  for i in 0..input_len {
+    nums_spoken[v[i]] = i+1;
   }
-  let mut max: usize = 0;
   let mut last: usize = 0;
-  for n in (v.len()+1)..30000000 {
-    let mut temp: usize =  0;
-    if nums_spoken.contains_key(&last) {
-      temp = n - nums_spoken[&last];
-    } else {
-      temp = 0;
-    }
-    nums_spoken.insert(last,n);
+  for n in (input_len+1)..max {
+    let temp: usize = 
+      if nums_spoken[last] > 0 {
+        n - nums_spoken[last]
+      } else {
+        0
+      };
+    nums_spoken[last] = n;
     last = temp;
-    if last > max {
-      max = last;
-    }
-    //println!("{}", last);
   }
-  println!("{}", max);
+  println!("{}", last);
 }
